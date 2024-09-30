a011cb5c1343e4e74b143e2713c0a86f3cbf26de
Saptashwa <saptashwa04@gmail.com>
2024-09-30 18:48:17 +0530

    added functionality

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ let arr = ["+/-", "C", "AC", "/", "7", "8", "9", "*", "4", "5", "6", "+",
 
 const grid = document.querySelector("#squares");
 let count = 0;
+let val1;
+let val2;
+let val3;
+let op;
+let len;
+
+function solve() {
+    if (op == "*") {
+        return val1 * val2;
+    } else if (op == "+") {
+        return val1 + val2;
+    } else if (op == "-") {
+        return val1 - val2;
+    } else {
+        if (val2 == 0) {
+            return "Cannot divide by 0";
+        }
+        return val1/val2;
+    }
+}
 
 for (let i=0; i<4; i++) {
     for (let j=0; j<5;j++) {
@@ -18,7 +38,27 @@ for (let i=0; i<4; i++) {
 
         sq.addEventListener("click", function() {
             let tex = document.querySelector("#t");
-            tex.value = tex.value + sq.textContent;
+            if (sq.textContent == "C") {
+                let str = new String(tex.value);
+                tex.value = str.substring(0,str.length-1);
+            } else if (sq.textContent == "AC") {
+                tex.value = "";
+            } else if (sq.textContent == "=") {
+                let st = new String(tex.value);
+                val2 = parseFloat(st.substring(len+1, st.length));
+                console.log("val3 = " + val2);
+                tex.value = solve();
+            }
+            else if (sq.textContent == "+" || sq.textContent == "-" || sq.textContent == "*" || sq.textContent == "/") {
+                val1 = parseFloat(tex.value);
+                op = sq.textContent;
+                len = new String(tex.value).length;
+                console.log("val1= " + val1);
+                console.log("op" + op);
+                tex.value = tex.value + sq.textContent;
+            } else {
+                tex.value = tex.value + sq.textContent;
+            }
         });
 
         count++;
